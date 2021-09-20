45aa8c8c059f13b665a42be9238e48887ff2fa25
DariaSeliverstova <dashulia2004@yandex.ru>
2021-09-20 18:54:13 +0300

    Changes Files

--- src/main/java/Manager/PostManager.java
+++ src/main/java/Manager/PostManager.java
@@ -5,11 +5,16 @@ import Domain.Post;
 public class PostManager {
     private Post[] posts;
 
-    public Post[] search() {
+    public  Post[] search(int...array) {
 
         return null;
     }
 
-    public void delete () {}
+    public Post [] search(String...array) {
+
+        return null;
+    }
+
+    public void delete (int...array) {}
 
 }
